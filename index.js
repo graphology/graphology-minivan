b99c788e775b3515220167b4746b870b0e9fec96
Yomguithereal <guillaumeplique@gmail.com>
2019-08-05 17:04:41 +0200

    Type inference

--- index.js
+++ index.js
@@ -29,13 +29,37 @@ var VALID_ATTR_TYPES = new Set([
   'ranking-color'
 ]);
 
-function optionOrAttribute(bundle, graph, name) {
-  if (options[name])
-    bundle[name] = options[name];
-  else if (graph.hasAttribute(name))
-    bundle[name] = graph.getAttribute(name);
+var TYPE_ORDER = {
+  string: 0,
+  float: 1,
+  integer: 2
+};
+
+function makeOptionOrAttribute(bundle, graph, options) {
+
+  return function(name) {
+    if (options.meta && options.meta[name])
+      bundle[name] = options.meta[name];
+    else if (graph.hasAttribute(name))
+      bundle[name] = graph.getAttribute(name);
+  };
 }
 
+function guessType(val) {
+  if (typeof val === 'string')
+    return 'string';
+
+  if (typeof val === 'number') {
+    if (val === (val | 0))
+      return 'integer';
+
+    return 'float';
+  }
+
+  return 'unknown';
+}
+
+// TODO: add option to sample data for type inference
 module.exports = function buildMinivanBundle(graph, options) {
 
   var bundle = {
@@ -43,17 +67,64 @@ module.exports = function buildMinivanBundle(graph, options) {
     consolidated: true
   };
 
-  // Gathering data
-  optionOrAttribute(bundle, graph, 'title');
-  optionOrAttribute(bundle, graph, 'description');
-  optionOrAttribute(bundle, graph, 'url');
-  optionOrAttribute(bundle, graph, 'date');
+  var optionOrAttribute = makeOptionOrAttribute(bundle, graph, options);
 
-  if (options.authors)
-    bundle.authors = options.authors;
+  // Gathering data
+  optionOrAttribute('title');
+  optionOrAttribute('description');
+  optionOrAttribute('url');
+  optionOrAttribute('date');
+  optionOrAttribute('authors'); // TODO: Might need some adjustments
 
   // Serializing graph data
-  bundle.graph = graph.export();
+  var serialized = graph.export();
+  bundle.graph = serialized;
+
+  // First pass for type inference
+  var nodeInferences = {},
+      edgeInferences = {};
+
+  var i, l, k, v, node, edge, attr, type, order;
+
+  for (var i = 0, l = serialized.nodes.length; i < l; i++) {
+    node = serialized.nodes[i];
+    attr = node.attributes;
+
+    for (k in attr) {
+      if (NODE_ATTRIBUTES_TO_IGNORE.has(k))
+        continue;
+
+      v = attr[k];
+
+      type = guessType(v);
+      order = TYPE_ORDER[type];
+
+      if (!(k in nodeInferences) || order < TYPE_ORDER[nodeInferences[k]])
+        nodeInferences[k] = type;
+    }
+  }
+
+  for (var i = 0, l = serialized.edges.length; i < l; i++) {
+    edge = serialized.edges[i];
+    attr = edge.attributes;
+
+    for (k in attr) {
+      if (EDGE_ATTRIBUTES_TO_IGNORE.has(k))
+        continue;
+
+      v = attr[k];
+
+      type = guessType(v);
+      order = TYPE_ORDER[type];
+
+      if (!(k in edgeInferences) || order < TYPE_ORDER[edgeInferences[k]])
+        edgeInferences[k] = type;
+    }
+  }
+
+  // Second pass to aggregate metrics and build model
+
+  console.log(nodeInferences, edgeInferences);
 
   return bundle;
 };
--- test.js
+++ test.js
@@ -2,4 +2,73 @@
  * Graphology Minivan Unit Tests
  * ==============================
  */
-// TODO...
+var assert = require('assert');
+var buildMinivanBundle = require('./index.js');
+var validate = require('./validate.js');
+var Graph = require('graphology');
+
+var UndirectedGraph = Graph.UndirectedGraph;
+
+var GRAPHS = {
+  basic: {
+    attributes: {
+      title: 'Basic Graph'
+    },
+    nodes: [
+      {
+        key: 'A',
+        attributes: {
+          nb: 14,
+          centrality: 0.8,
+          color: 'red',
+          category: 'fruit'
+        }
+      },
+      {
+        key: 'B',
+        attributes: {
+          nb: 67,
+          centrality: -18.74,
+          color: 'blue',
+          category: 'vegetable'
+        }
+      },
+      {
+        key: 'C',
+        attributes: {
+          nb: 542,
+          centrality: 13,
+          color: 'red',
+          category: 'fruit'
+        }
+      }
+    ],
+    edges: [
+      {
+        source: 'A',
+        target: 'B',
+        attributes: {
+          weight: 0.556,
+          cardinality: 34,
+          predicate: 'HAS'
+        },
+        undirected: true
+      }
+    ]
+  }
+};
+
+describe('graphology-minivan', function() {
+  describe('serialization', function() {
+
+    it('should produce a correct bundle.', function() {
+      var graph = UndirectedGraph.from(GRAPHS.basic);
+
+      var bundle = buildMinivanBundle(graph, {meta: {url: 'http://supergraph.sv'}});
+
+      var errors = validate(bundle);
+
+      assert(!errors);
+    });
+  });
+});
