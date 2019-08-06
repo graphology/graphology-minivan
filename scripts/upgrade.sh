b7e2d1eb31f5af304da8ef62b38020733cfb24b9
Yomguithereal <guillaumeplique@gmail.com>
2019-08-06 17:08:36 +0200

    Basic upgrade script for easy iteration

---
+++ scripts/upgrade.sh
@@ -0,0 +1,8 @@
+MINIVAN=~/code/minivan/app
+OLD=$MINIVAN/olddata
+NEW=$MINIVAN/data
+
+node scripts/convert-alpha.js $OLD/test00.json > $NEW/test00.json
+node scripts/convert-alpha.js $OLD/test01.json > $NEW/test01.json
+node scripts/convert-alpha.js $OLD/test02.json > $NEW/test02.json
+node scripts/convert-alpha.js $OLD/test03.json > $NEW/test03.json
